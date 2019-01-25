ec78e131e5ade3cd15e7cdaafebcbde14590e5d8
Alena Pavlova <alenapavlova@lodossteam.com>
2019-01-25 17:16:57 +0300

    Add template in footer

--- pwa-studio/packages/venia-concept/src/components/Footer/footer.js
+++ pwa-studio/packages/venia-concept/src/components/Footer/footer.js
@@ -19,49 +19,78 @@ class Footer extends Component {
         const { classes } = this.props;
 
         return (
-            <footer className={classes.root}>
-                <div className={classes.tile}>
-                    <h2 className={classes.tileTitle}>
-                        <span>Your Account</span>
-                    </h2>
-                    <p className={classes.tileBody}>
-                        <span>
-                            Sign up and get access to our wonderful rewards
-                            program.
-                        </span>
-                    </p>
-                </div>
-                <div className={classes.tile}>
-                    <h2 className={classes.tileTitle}>
-                        <span>inquiries@example.com</span>
-                    </h2>
-                    <p className={classes.tileBody}>
-                        <span>
-                            Need to email us? Use the address above and
-                            we&rsquo;ll respond as soon as possible.
-                        </span>
-                    </p>
-                </div>
-                <div className={classes.tile}>
-                    <h2 className={classes.tileTitle}>
-                        <span>Live Chat</span>
-                    </h2>
-                    <p className={classes.tileBody}>
-                        <span>Mon – Fri: 5 a.m. – 10 p.m. PST</span>
-                        <br />
-                        <span>Sat – Sun: 6 a.m. – 9 p.m. PST</span>
-                    </p>
-                </div>
-                <div className={classes.tile}>
-                    <h2 className={classes.tileTitle}>
-                        <span>Help Center</span>
-                    </h2>
-                    <p className={classes.tileBody}>
-                        <span>Get answers from our community online.</span>
-                    </p>
-                </div>
-                <div className={classes.copyright}>
-                    <span>© Magento 2018. All rights reserved.</span>
+            <footer class="footer">
+                <div class="container">
+                    <div class="row">
+
+                        <div class="col-lg-3 footer_col">
+                            <div class="footer_column footer_contact">
+                                <div class="logo_container">
+                                    <div class="logo"><a href="#">OneTech</a></div>
+                                </div>
+                                <div class="footer_title">Got Question? Call Us 24/7</div>
+                                <div class="footer_phone">+38 068 005 3570</div>
+                                <div class="footer_contact_text">
+                                    <p>17 Princess Road, London</p>
+                                    <p>Grester London NW18JR, UK</p>
+                                </div>
+                                <div class="footer_social">
+                                    <ul>
+                                        <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-google"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-vimeo-v"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-2 offset-lg-2">
+                            <div class="footer_column">
+                                <div class="footer_title">Find it Fast</div>
+                                <ul class="footer_list">
+                                    <li><a href="#">Computers and Laptops</a></li>
+                                    <li><a href="#">Cameras and Photos</a></li>
+                                    <li><a href="#">Hardware</a></li>
+                                    <li><a href="#">Smartphones and Tablets</a></li>
+                                    <li><a href="#">TV and Audio</a></li>
+                                </ul>
+                                <div class="footer_subtitle">Gadgets</div>
+                                <ul class="footer_list">
+                                    <li><a href="#">Car Electronics</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-2">
+                            <div class="footer_column">
+                                <ul class="footer_list footer_list_2">
+                                    <li><a href="#">Video Games and Consoles</a></li>
+                                    <li><a href="#">Accessories</a></li>
+                                    <li><a href="#">Cameras and Photos</a></li>
+                                    <li><a href="#">Hardware</a></li>
+                                    <li><a href="#">Computers and Laptops</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-2">
+                            <div class="footer_column">
+                                <div class="footer_title">Customer Care</div>
+                                <ul class="footer_list">
+                                    <li><a href="#">My Account</a></li>
+                                    <li><a href="#">Order Tracking</a></li>
+                                    <li><a href="#">Wish List</a></li>
+                                    <li><a href="#">Customer Services</a></li>
+                                    <li><a href="#">Returns / Exchange</a></li>
+                                    <li><a href="#">FAQs</a></li>
+                                    <li><a href="#">Product Support</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </footer>
         );
